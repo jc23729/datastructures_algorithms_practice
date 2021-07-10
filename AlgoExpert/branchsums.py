42d9ba3d8a314b7506fc3b18f0200a8e280d1292
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-07-10 15:36:44 -0400

    Create branchsums.py

---
+++ AlgoExpert/branchsums.py
@@ -0,0 +1,8 @@
+# Write a function that takes in a binary tree and returns a list of its branch sum ordered from leftmost to rightmost branch sum.
+# A branch is a sum of all the values in a binary tree branch, 
+# a binary tree branch is a path of nodes in a tree that starts at the root node and ends at any leaf node
+
+# Each BinaryTree node has an integer value, a left child node, and a right child node.
+# Children nodes can either be BinaryTree nodes themselves or None/null
+
+
